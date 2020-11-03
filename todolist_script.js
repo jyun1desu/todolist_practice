1533668d641175b43383261feef846eff6d8507b
jyun1 <jyuny12345@gmail.com>
2020-11-03 13:33:22 +0800

    populate task

--- todolist_script.js
+++ todolist_script.js
@@ -4,16 +4,25 @@ const addTaskForm = document.querySelector('.add_task_form');
 const cancelButton = document.querySelector('.cancel_button');
 const updateFile = document.querySelector('#file_update');
 const fileName = document.querySelector('.file_name');
+//完成狀態
+const status = document.querySelector('input#status');
+//打星星
 const priority = document.querySelector('#priority');
+//編輯中
+const editting = document.querySelector('#edit');
 //剩餘任務數量
 const countLeft = document.querySelector('.left_tasks_numbers');
-
+//任務清單
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const taskList = document.querySelector('.todo_list');
 
 function newTask() {
     this.classList.add('click');
     addTaskForm.classList.add('click');
     setTimeout(() => addTaskForm.classList.add('click-active'), 5)
+    addTaskForm.querySelector('#edit').checked = true;
+    addTaskForm.querySelector('label[for="edit"]').innerHTML = `<i class="fas fa-pen"></i>`;
+
 }
 
 function addTask(e) {
@@ -41,19 +50,18 @@ function addTask(e) {
     }
 
     tasks.push(task);
-    this.querySelector('[name="title"]').placeholder = "Type Something Here...";
-    this.querySelector('[name="title"]').style.setProperty("--c", "#000000");
     localStorage.setItem('tasks', JSON.stringify(tasks));
-    this.querySelector('.title_area').classList.remove('marked');
-    this.querySelector('label[for="priority"]').innerHTML = `<i class="far fa-star">`;
     this.reset();
+    resetForm();
 }
 
-function cancelAdding(e) {
-    e.preventDefault();
-    addTaskForm.reset();
+function resetForm() {
+    addTaskForm.querySelector('[name="title"]').placeholder = "Type Something Here...";
+    addTaskForm.querySelector('[name="title"]').style.setProperty("--c", "#000000");
     addTaskForm.querySelector('.title_area').classList.remove('marked');
+    addTaskForm.querySelector('.title_area').classList.remove('task-done');
     addTaskForm.querySelector('label[for="priority"]').innerHTML = `<i class="far fa-star">`;
+    withDraw();
 }
 
 function updateName() {
@@ -77,13 +85,74 @@ function focus() {
     this.classList.add('focus-active');
 }
 
+function toggleDone(e) {
+    const checkbox = this.parentNode;
+    if (this.checked) checkbox.classList.add('task-done');
+    else(checkbox.classList.remove('task-done'))
+}
 
+function withDraw() {
+    addTaskForm.classList.remove('click-active');
+    setTimeout(() => addTaskForm.classList.remove('click'), 280);
+    setTimeout(() => addTaskButton.classList.remove('click'), 280);
+}
+
+function handleEdit() {
+    const editMark = this.parentNode.querySelector('label[for="edit"]');
+    if (this.checked) {
+        editMark.innerHTML = `<i class="fas fa-pen"></i>`;
+    } else {
+        editMark.innerHTML = `<i class="far fa-pen"></i>`;
+        addTaskForm.classList.remove('click-active');
+        setTimeout(() => addTaskForm.classList.remove('click'), 280);
+        setTimeout(() => addTaskButton.classList.remove('click'), 280);
+    }
+}
+
+function populateList(tasks = [], taskList) {
+    taskList.innerHTML = tasks.map(task => {
+        return `
+        <li class="tasks ${task.primary?"marked":""}">
+            <div class="title_overview">
+                <div class="main_information">
+                    <input id="status" class="completed_checkbox" type="checkbox" ${task.done?"checked":" "}>
+                    <label for="status"><i class="fas fa-check"></i></label>
+                    <p class="task_title">${task.taskTitle}</p>
+                    <input id="priority" type="checkbox" ${task.primary?"checked":" "}>
+                    <label for="priority" class="star_mark"><i class="${task.primary?"fas":"far"} fa-star"></i></label>
+                    <input id="edit" type="checkbox">
+                    <label for="edit" class="edit_icon"><i class="far fa-pen"></i><label>
+                </div>
+
+                <div class="detail">
+                ${task.deadlineDate?`<span>
+                <i class="far fa-calendar-alt"></i>
+                <span>${task.deadlineDate}</span></span>`:""}
+                ${task.updateFile?`<span><i class="far fa-file"></i></span>`:""}
+                ${task.memo?`<span><i class="far fa-comment-dots"></i></span>`:""}
+                    </div>
+                </div>
+        </li>
+        `
+    }).join("");
+}
+
+
+//navbar分類點擊
 navButtons.forEach(button => button.addEventListener("click", focus));
+//新增任務
 addTaskButton.addEventListener("click", newTask);
 addTaskForm.addEventListener("submit", addTask);
-cancelButton.addEventListener("click", cancelAdding);
+cancelButton.addEventListener("click", resetForm);
 updateFile.addEventListener("change", updateName)
 priority.addEventListener("change", markPriority);
+status.addEventListener("change", toggleDone);
+editting.addEventListener("input", handleEdit);
+//點擊外圍收回新增表單
+window.addEventListener("click", (e) => {
+    if (e.target === document.body) withDraw();
+});
+populateList(tasks,taskList);
 
-
-countLeft.textContent=`${tasks.filter(task=>task.done===false).length} task${tasks.filter(task=>task.done===false).length>1?"s":""} left`
+//計算剩餘任務數量
+countLeft.textContent = `${tasks.filter(task=>task.done===false).length} task${tasks.filter(task=>task.done===false).length>1?"s":""} left`
