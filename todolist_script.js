7fb26ef6354eb65be871830c42dd1f60ff3cd331
jyun1 <jyuny12345@gmail.com>
2020-11-04 10:31:41 +0800

    input js

--- todolist_script.js
+++ todolist_script.js
@@ -109,22 +109,36 @@ function handleEditButton() {
     }
 }
 
+function hello(element) {
+    const label = element.nextElementSibling;
+    const task = element.parentNode.parentNode.parentNode;
+    const index = task.dataset.index;
+
+    label.classList.toggle("clicked");
+    if(element.dataset.use){
+        const usage = element.dataset.use;
+        tasks[index][usage] = !tasks[index][usage]
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    console.log(tasks[index])
+}
+
 function populateList(tasks = [], taskList) {
-    taskList.innerHTML = tasks.map((task,index) => {
+    taskList.innerHTML = tasks.map((task, index) => {
         return `
         <form data-index="${index}" class="tasks ${task.primary?"marked":""} ${task.done?"task-done":""}">
             <div class="title_overview">
                 <div class="main_information">
-                    <input id="status${index}" class="completed_checkbox" type="checkbox" ${task.done?"checked":" "}>
-                    <label for="status${index}" class="completed_checkbox" ><i class="fas fa-check"></i></label>
+                    <input id="status${index}" data-use="done" onclick="hello(this)" class="completed_checkbox" type="checkbox" ${task.done?"checked":" "}>
+                    <label for="status${index}" class="completed_checkbox  ${task.done?"clicked":""}" ><i class="fas fa-check"></i></label>
                     <p class="task_title">${task.taskTitle}</p>
-                    <input id="priority${index}" class="star_mark" type="checkbox" ${task.primary?"checked":" "}>
-                    <label for="priority${index}" class="star_mark"><i class="${task.primary?"fas":"far"} fa-star"></i></label>
-                    <input id="edit${index}" class="edit_icon" type="checkbox">
+                    <input id="priority${index}" data-use="primary"  onclick="hello(this)" class="star_mark" type="checkbox" ${task.primary?"checked":" "}>
+                    <label for="priority${index}" class="star_mark ${task.primary?"clicked":""}"><i class="${task.primary?"fas":"far"} fa-star"></i></label>
+                    <input id="edit${index}" onclick="hello(this)" class="edit_icon" type="checkbox">
                     <label for="edit${index}" class="edit_icon"><i class="far fa-pen"></i><label>
                 </div>
 
-                <div class="detail">
+                <div class="quick_detail">
                 ${task.deadlineDate?`<span>
                 <i class="far fa-calendar-alt"></i>
                 <span>${task.deadlineDate}</span></span>`:""}
@@ -137,45 +151,98 @@ function populateList(tasks = [], taskList) {
     }).join("");
 }
 
-function handleTaskEdit(e) {
-    if (!e.target.matches('i')) return;
-    const input = e.target.parentNode.previousElementSibling;
-    const inputType = input.classList[0];
-    const task = input.parentNode.parentNode.parentNode;
-    switch (inputType) {
-        case "completed_checkbox":
-            task.classList.toggle('task-done');
-            tasks[task.dataset.index].done = !tasks[task.dataset.index].done
-            break;
-        case "star_mark":
-            task.classList.toggle('marked');
-            tasks[task.dataset.index].primary = !tasks[task.dataset.index].primary
-            if (input.checked) {
-                e.target.parentNode.innerHTML = `<i class="far fa-star"></i>`;
-            }else{
-                e.target.parentNode.innerHTML = `<i class="fas fa-star"></i>`;
-            }
-            break;
-        case "edit_icon":
-            if (input.checked) {
-                e.target.parentNode.innerHTML = `<i class="far fa-pen"></i>`;
-                
-            } else {
-                e.target.parentNode.innerHTML = `<i class="fas fa-pen"></i>`;
-            }
-            break;
-    }
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
+// function handleTaskEdit(e) {
+//     if (!e.target.matches('i')) return;
+//     const input = e.target.parentNode.previousElementSibling;
+//     const inputType = input.classList[0];
+//     const task = input.parentNode.parentNode.parentNode;
+//     switch (inputType) {
+//         case "completed_checkbox":
+//             task.classList.toggle('task-done');
+//             tasks[task.dataset.index].done = !tasks[task.dataset.index].done
+//             break;
+//         case "star_mark":
+//             task.classList.toggle('marked');
+//             tasks[task.dataset.index].primary = !tasks[task.dataset.index].primary
+//             if (input.checked) {
+//                 e.target.parentNode.innerHTML = `<i class="far fa-star"></i>`;
+//             } else {
+//                 e.target.parentNode.innerHTML = `<i class="fas fa-star"></i>`;
+//             }
+//             break;
+//         case "edit_icon":
+//             if (input.checked) {
+//                 e.target.parentNode.innerHTML = `<i class="far fa-pen"></i>`;
+//                 // detailWithDraw(task);
+//             } else {
+//                 e.target.parentNode.innerHTML = `<i class="fas fa-pen"></i>`;
+//                 detailDropdown(task);
+//             }
+//             break;
+//     }
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+// }
 
-// populateList(tasks, taskList);
+// function detailDropdown(element) {
+//     const index = element.dataset.index;
+//     const task = tasks[index];
+//     element.innerHTML = `<div class="title_overview">
+//     <div class="main_information">
+//         <input id="status${index}" class="completed_checkbox" type="checkbox" ${task.done?"checked":" "}>
+//         <label for="status${index}" class="completed_checkbox" ><i class="fas fa-check"></i></label>
+//         <p class="task_title">${task.taskTitle}</p>
+//         <input id="priority${index}" class="star_mark" type="checkbox" ${task.primary?"checked":" "}>
+//         <label for="priority${index}" class="star_mark"><i class="${task.primary?"fas":"far"} fa-star"></i></label>
+//         <input id="edit${index}" class="edit_icon" type="checkbox" checked>
+//         <label for="edit${index}" class="edit_icon"><i class="fas fa-pen"></i><label>
+//     </div>
+
+//     <div class="detail_area">
+//         <div class="deadline">
+//             <i class="icon far fa-calendar-alt"></i>
+//             <div class="content_block">
+//                 <p>Deadline</p>
+//                 <div class="time_block">
+//                     <input name="date" class="deadline_date" value="${task.deadlineDate}" type="date" placeholder="yyyy/mm/dd">
+//                     <input name="time" class="deadline_time" value="${task.deadlineTime}" type="time" placeholder="hh:mm">
+//                 </div>
+//             </div>
+//         </div>
+//     <div class="file_update">
+//         <i class="icon far fa-file"></i>
+//         <div class="content_block">
+//             <p>File</p>
+//             <input id="file_update" name="update" type="file" class="update_button" value="">
+//             ${task.updateFile?`<span class="file_name">${task.updateFile}</span>`:""}
+//             <label for="file_update"></label>
+//         </div>
+
+//     </div>
+//     <div class="memo">
+//         <i class="icon far fa-comment-dots"></i>
+//         <div class="content_block">
+//             <p>Comment</p>
+//             <textarea name="memo_content" placeholder="Type your memo here...">${task.memo}</textarea>
+//         </div>
+//     </div>
+
+// </div>
+
+// <div class="button_area">
+//     <button type="reset" class="cancel_button">&times; Cancel</button>
+//     <button type="submit" class="submit_button">&#43; Save</button>
+// </div>`
+
+// }
+
+populateList(tasks, taskList);
 //navbar分類點擊
 navButtons.forEach(button => button.addEventListener("click", focus));
 //新增任務
 addTaskButton.addEventListener("click", newTask);
 addTaskForm.addEventListener("submit", addTask);
 cancelButton.addEventListener("click", resetForm);
-updateFile.addEventListener("change", updateName)
+updateFile.addEventListener("change", updateName);
 priority.addEventListener("change", markPriority);
 status.addEventListener("change", toggleDone);
 editting.addEventListener("input", handleEditButton);
@@ -185,7 +252,7 @@ window.addEventListener("click", (e) => {
 });
 
 
-taskList.addEventListener("click", handleTaskEdit);
+// taskList.addEventListener("click", handleTaskEdit);
 
 //計算剩餘任務數量
 countLeft.textContent = `${tasks.filter(task=>task.done===false).length} task${tasks.filter(task=>task.done===false).length>1?"s":""} left`
