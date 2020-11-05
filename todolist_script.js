e0e78b32b140ed32d46282c8b18acb83b1deaa27
jyun1 <jyuny12345@gmail.com>
2020-11-05 13:38:25 +0800

    sort and populate the task list

--- todolist_script.js
+++ todolist_script.js
@@ -15,10 +15,84 @@ const taskList = document.querySelector('.todo_list');
 let taskOrdinal = 0
 //編輯任務
 
+
+
+const taskHTMLlist = tasks.map((task, index) => {
+    const eachTaskHTML = `<form data-index="${index}" class="tasks ${task.primary?"primary":""} ${task.done?"done":""}" draggable="true">
+    <div class="drag_icon">
+    </div>
+    <div class="main_information">
+    <input id="status${index}" data-use="done" onclick="toggleStatus(this)" class="completed_checkbox" type="checkbox"
+        ${task.done?"checked":" "}>
+                    <label for="status${index}" class="completed_checkbox  ${task.done?" clicked":""}"><i
+        class="fas fa-check"></i></label>
+    <input type="text" class="task_title" value="${task.taskTitle}" placeholder="Type Something Here..." readonly>
+    <input id="priority${index}" data-use="primary" onclick="toggleStatus(this)" class="star_mark" type="checkbox"
+        ${task.primary?"checked":" "}>
+                    <label for="priority${index}" class="star_mark ${task.primary?" clicked":""}"><i
+        class="${task.primary?"fas":"far"} fa-star"></i></label>
+    <input id="edit${index}" onclick="toggleShow(this)" class="edit_icon" type="checkbox">
+    <label for="edit${index}" class="edit_icon"><i class="far fa-pen"></i></label>
+    
+    <input id="delete${index}" class="delete_icon" type="checkbox">
+    <label for="delete${index}" onclick="deleteTask(this)" class="delete_icon"><i class="far fa-trash-alt"></i></label>
+    </div>
+    
+    <div class="quick_detail">
+    ${task.deadlineDate?`<span>
+        <i class="far fa-calendar-alt"></i>
+        <span>${task.deadlineDate}</span></span>`:""}
+    ${task.updateFile?`<span><i class="far fa-file"></i></span>`:""}
+    ${task.memo?`<span><i class="far fa-comment-dots"></i></span>`:""}
+    </div>
+    
+    <div class="detail_area" style="display:none;">
+    <div class="deadline">
+        <i class="icon far fa-calendar-alt"></i>
+        <div class="content_block">
+            <p>Deadline</p>
+            <div class="time_block">
+                <input name="date" class="deadline_date" value="${task.deadlineDate}" type="date"
+                    placeholder="yyyy/mm/dd">
+                <input name="time" class="deadline_time" value="${task.deadlineTime}" type="time" placeholder="hh:mm">
+            </div>
+        </div>
+    </div>
+    <div class="file_update">
+        <i class="icon far fa-file"></i>
+        <div class="content_block">
+            <p>File</p>
+            <input id="file_update${index}" onchange="fileNameUpdate(this)" name="update" type="file" class="update_button">
+            <span class="file_name">${task.updateFile||""}</span>
+            <label for="file_update${index}"></label>
+        </div>
+    </div>
+    <div class="memo">
+        <i class="icon far fa-comment-dots"></i>
+        <div class="content_block">
+            <p>Comment</p>
+            <textarea name="memo_content" placeholder="Type your memo here...">${task.memo}</textarea>
+        </div>
+    </div>
+    
+    </div>
+    <div class="button_area" style="display:none;">
+    <button type="button" class="cancel_edit_button">&times; Cancel</button>
+    <button type="button" class="save_button">&#43; Save</button>
+    </div>
+    </form>`
+    return eachTaskHTML
+})
+
+const sortedPrimary = taskHTMLlist.filter(el=>el.includes(`tasks primary`)&&!el.includes(`tasks primary done`)).join("");
+const sortedNormal = taskHTMLlist.filter(el=>el.includes(`class="tasks  "`)).join("");
+const sortedDonePrimary = taskHTMLlist.filter(el=>el.includes(`tasks primary done`)).join("");
+const sortedDoneNormal = taskHTMLlist.filter(el=>el.includes(`tasks  done`)).join("");
+
+
 function populateList(tasks = [], taskList) {
-    taskList.innerHTML = tasks.map((task, index) => {
-        return `
-        <form data-index="${index}" class="tasks ${task.primary?"primary":""} ${task.done?"done":""}" draggable="true">
+    const taskHTMLlist = tasks.map((task, index) => {
+        const eachTaskHTML = `<form data-index="${index}" class="tasks ${task.primary?"primary":""} ${task.done?"done":""}" draggable="true">
         <div class="drag_icon">
         </div>
         <div class="main_information">
@@ -33,20 +107,20 @@ function populateList(tasks = [], taskList) {
             class="${task.primary?"fas":"far"} fa-star"></i></label>
         <input id="edit${index}" onclick="toggleShow(this)" class="edit_icon" type="checkbox">
         <label for="edit${index}" class="edit_icon"><i class="far fa-pen"></i></label>
-
+        
         <input id="delete${index}" class="delete_icon" type="checkbox">
         <label for="delete${index}" onclick="deleteTask(this)" class="delete_icon"><i class="far fa-trash-alt"></i></label>
-    </div>
-    
-    <div class="quick_detail">
+        </div>
+        
+        <div class="quick_detail">
         ${task.deadlineDate?`<span>
             <i class="far fa-calendar-alt"></i>
             <span>${task.deadlineDate}</span></span>`:""}
         ${task.updateFile?`<span><i class="far fa-file"></i></span>`:""}
         ${task.memo?`<span><i class="far fa-comment-dots"></i></span>`:""}
-    </div>
-    
-    <div class="detail_area" style="display:none;">
+        </div>
+        
+        <div class="detail_area" style="display:none;">
         <div class="deadline">
             <i class="icon far fa-calendar-alt"></i>
             <div class="content_block">
@@ -74,15 +148,33 @@ function populateList(tasks = [], taskList) {
                 <textarea name="memo_content" placeholder="Type your memo here...">${task.memo}</textarea>
             </div>
         </div>
+        
+        </div>
+        <div class="button_area" style="display:none;">
+        <button type="button" class="cancel_edit_button">&times; Cancel</button>
+        <button type="button" class="save_button">&#43; Save</button>
+        </div>
+        </form>`
+        return eachTaskHTML
+    })
+    
+    const sortedPrimary = taskHTMLlist.filter(el=>el.includes(`tasks primary`)&&!el.includes(`tasks primary done`)).join("");
+    const sortedNormal = taskHTMLlist.filter(el=>el.includes(`class="tasks  "`)).join("");
+    const sortedDonePrimary = taskHTMLlist.filter(el=>el.includes(`tasks primary done`)).join("");
+    const sortedDoneNormal = taskHTMLlist.filter(el=>el.includes(`tasks  done`)).join("");
 
-    </div>
-    <div class="button_area" style="display:none;">
-    <button type="button" class="cancel_edit_button">&times; Cancel</button>
-    <button type="button" class="save_button">&#43; Save</button>
-</div>
-        </form>
-        `
-    }).join("");
+    const primaryBlock = taskList.querySelector('#primary_category');
+    const normalBlock = taskList.querySelector('#normal_category');
+    const donePrimaryBlock = taskList.querySelector('#done_primary_category');
+    const doneNormalBlock = taskList.querySelector('#done_normal_category');
+
+    primaryBlock.innerHTML = sortedPrimary;
+    normalBlock.innerHTML = sortedNormal;
+    donePrimaryBlock.innerHTML = sortedDonePrimary;
+    doneNormalBlock.innerHTML = sortedDoneNormal;
+
+
+    console.log(taskList.querySelector('#primary_category'))
     countLeft.textContent = `${tasks.filter(task=>task.done===false).length} task${tasks.filter(task=>task.done===false).length>1?"s":""} left`
 }
 
@@ -264,7 +356,7 @@ function editTask(e) {
     }
 }
 
-function deleteTask(element){
+function deleteTask(element) {
     console.log(element)
 }
 populateList(tasks, taskList);
