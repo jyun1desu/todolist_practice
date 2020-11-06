55cf22cc241023803fa04799da2c8fb538a80930
jyun1 <jyuny12345@gmail.com>
2020-11-06 11:34:14 +0800

    local strorage the order andpopulate orderred list when refreshing

--- todolist_script.js
+++ todolist_script.js
@@ -12,6 +12,7 @@ const countLeft = document.querySelector('.left_tasks_numbers');
 //任務清單
 const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 const taskList = document.querySelector('.todo_list');
+const orderArray = JSON.parse(localStorage.getItem('order'))
 //編輯任務
 
 const droppableAreas = Array.from(document.querySelectorAll('.droppable_area'));
@@ -24,7 +25,6 @@ droppableAreas.forEach(area => {
 let draggedElement;
 let currentPassbyElement;
 let previousY;
-let orderArray;
 
 //拖曳物件
 function handleDragStart(event, element) {
@@ -71,14 +71,97 @@ function handleDrop(e) {
         dropdownArea.insertBefore(draggedElement, currentPassbyElement)
     }
 
-    orderArray = Array.from(document.querySelectorAll('.tasks'))
-        .map((el) => tasks[el.dataset.index]);
+    const orderArray = Array.from(document.querySelectorAll('.tasks'))
+        .map((el) => el.dataset.index);
     localStorage.setItem('order', JSON.stringify(orderArray));
-
 }
 
+// function populateList(tasksArray = [], taskList) {
+//     const taskHTMLlist = tasksArray.map((task, index) => {
+//         const eachTaskHTML = `<form data-index="${index}" ondragstart="handleDragStart(event,this)" ondragend="handleDragEnd(event,this)" ondragenter="handleDragPassby(event,this)" class="tasks ${task.primary?"primary":""} ${task.done?"done":""}" draggable="true">
+//         <div class="drag_icon">
+//         </div>
+//         <div class="main_information">
+//         <input id="status${index}" data-use="done" onclick="toggleStatus(this)" class="completed_checkbox" type="checkbox"
+//             ${task.done?"checked":" "}>
+//                         <label for="status${index}" class="completed_checkbox  ${task.done?" clicked":""}"><i
+//             class="fas fa-check"></i></label>
+//         <input type="text" class="task_title" value="${task.taskTitle}" placeholder="Type Something Here..." readonly>
+//         <input id="priority${index}" data-use="primary" onclick="toggleStatus(this)" class="star_mark" type="checkbox"
+//             ${task.primary?"checked":" "}>
+//                         <label for="priority${index}" class="star_mark ${task.primary?" clicked":""}"><i
+//             class="${task.primary?"fas":"far"} fa-star"></i></label>
+//         <input id="edit${index}" onclick="toggleShow(this)" class="edit_icon" type="checkbox">
+//         <label for="edit${index}" class="edit_icon"><i class="far fa-pen"></i></label>
+
+//         <input id="delete${index}" class="delete_icon" type="checkbox">
+//         <label for="delete${index}" onclick="deleteTask(this)" class="delete_icon"><i class="far fa-trash-alt"></i></label>
+//         </div>
+
+//         <div class="quick_detail">
+//         ${task.deadlineDate?`<span>
+//             <i class="far fa-calendar-alt"></i>
+//             <span>${task.deadlineDate}</span></span>`:""}
+//         ${task.updateFile?`<span><i class="far fa-file"></i></span>`:""}
+//         ${task.memo?`<span><i class="far fa-comment-dots"></i></span>`:""}
+//         </div>
+
+//         <div class="detail_area" style="display:none;">
+//         <div class="deadline">
+//             <i class="icon far fa-calendar-alt"></i>
+//             <div class="content_block">
+//                 <p>Deadline</p>
+//                 <div class="time_block">
+//                     <input name="date" class="deadline_date" value="${task.deadlineDate}" type="date"
+//                         placeholder="yyyy/mm/dd">
+//                     <input name="time" class="deadline_time" value="${task.deadlineTime}" type="time" placeholder="hh:mm">
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="file_update">
+//             <i class="icon far fa-file"></i>
+//             <div class="content_block">
+//                 <p>File</p>
+//                 <input id="file_update${index}" onchange="fileNameUpdate(this)" name="update" type="file" class="update_button">
+//                 <span class="file_name">${task.updateFile||""}</span>
+//                 <label for="file_update${index}"></label>
+//             </div>
+//         </div>
+//         <div class="memo">
+//             <i class="icon far fa-comment-dots"></i>
+//             <div class="content_block">
+//                 <p>Comment</p>
+//                 <textarea name="memo_content" placeholder="Type your memo here...">${task.memo}</textarea>
+//             </div>
+//         </div>
+
+//         </div>
+//         <div class="button_area" style="display:none;">
+//         <button type="button" class="cancel_edit_button">&times; Cancel</button>
+//         <button type="button" class="save_button">&#43; Save</button>
+//         </div>
+//         </form>`
+//         return eachTaskHTML
+//     })
+
+//     const sortedPrimary = taskHTMLlist.filter(el => el.includes(`tasks primary`) && !el.includes(`tasks primary done`)).join("");
+//     const sortedNormal = taskHTMLlist.filter(el => el.includes(`class="tasks  "`)).join("");
+//     const sortedDonePrimary = taskHTMLlist.filter(el => el.includes(`tasks primary done`)).join("");
+//     const sortedDoneNormal = taskHTMLlist.filter(el => el.includes(`tasks  done`)).join("");
+//     const primaryBlock = taskList.querySelector('#primary_category');
+//     const normalBlock = taskList.querySelector('#normal_category');
+//     const donePrimaryBlock = taskList.querySelector('#done_primary_category');
+//     const doneNormalBlock = taskList.querySelector('#done_normal_category');
+
+//     primaryBlock.innerHTML = sortedPrimary;
+//     normalBlock.innerHTML = sortedNormal;
+//     donePrimaryBlock.innerHTML = sortedDonePrimary;
+//     doneNormalBlock.innerHTML = sortedDoneNormal;
+//     countLeft.textContent = `${tasks.filter(task=>task.done===false).length} task${tasks.filter(task=>task.done===false).length>1?"s":""} left`
+// }
+
 function populateList(tasksArray = [], taskList) {
-    const taskHTMLlist = tasksArray.map((task, index) => {
+    let taskHTMLlist = tasksArray.map((task, index) => {
         const eachTaskHTML = `<form data-index="${index}" ondragstart="handleDragStart(event,this)" ondragend="handleDragEnd(event,this)" ondragenter="handleDragPassby(event,this)" class="tasks ${task.primary?"primary":""} ${task.done?"done":""}" draggable="true">
         <div class="drag_icon">
         </div>
@@ -144,7 +227,9 @@ function populateList(tasksArray = [], taskList) {
         </form>`
         return eachTaskHTML
     })
-
+    // if(orderArray){
+    //     taskHTMLlist = orderArray.map(order => taskHTMLlist[order]);
+    // }
     const sortedPrimary = taskHTMLlist.filter(el => el.includes(`tasks primary`) && !el.includes(`tasks primary done`)).join("");
     const sortedNormal = taskHTMLlist.filter(el => el.includes(`class="tasks  "`)).join("");
     const sortedDonePrimary = taskHTMLlist.filter(el => el.includes(`tasks primary done`)).join("");
@@ -263,9 +348,8 @@ function toggleStatus(element) {
     if (task.dataset.index) {
         tasks[index][usage] = !tasks[index][usage];
     }
-    
+
     localStorage.setItem('tasks', JSON.stringify(tasks));
-    localStorage.setItem('order', JSON.stringify(orderArray));
     populateList(tasks, taskList);
     countLeft.textContent = `${tasks.filter(task=>task.done===false).length} task${tasks.filter(task=>task.done===false).length>1?"s":""} left`
 }
@@ -344,7 +428,7 @@ function editTask(e) {
 
 function deleteTask(element) {
     const index = element.previousElementSibling.id.match(/\d+/);
-    tasks.splice(index,1)
+    tasks.splice(index, 1)
     localStorage.setItem('tasks', JSON.stringify(tasks));
     populateList(tasks, taskList);
 
